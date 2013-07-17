02ef94df2250e88d5e24719a98568d606d8dc651
Ubuntu <ubuntu@domU-12-31-39-02-90-69.compute-1.internal>
2013-07-17 21:19:57 +0000

    first commit

--- setup.sh
+++ setup.sh
@@ -2,35 +2,47 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
 
-# Install nvm: node-version manager
+## Install node and npm
+sudo apt-get update
+# Install a special package
+sudo apt-get install -y python-software-properties python g++ make
+# Add a new repository for apt-get to search
+sudo add-apt-repository ppa:chris-lea/node.js
+# Update apt-get's knowledge of which packages are where
+sudo apt-get update
+# Now install nodejs and npm
+sudo apt-get install -y nodejs
+sudo apt-get install npm
+
+## Install nvm: node-version manager
 # https://github.com/creationix/nvm
 sudo apt-get install -y git-core
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
+## Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
 nvm install v0.10.12
 nvm use v0.10.12
 
-# Install jshint to allow checking of JS code within emacs
+## Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
 npm install -g jshint
 
-# Install rlwrap to provide libreadline features with node
+## Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
 
-# Install emacs24
+## Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
 sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
-# Install Heroku toolbelt
+## Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
-# git pull and install dotfiles as well
+## git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
